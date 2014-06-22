5d609e3c578f2462972e50faee7aa811a373803a
Evan Dempsey <evandempsey@gmail.com>
2014-06-22 16:40:31 +0100

    Setting up options parsing.

--- supervisor.c
+++ supervisor.c
@@ -2,9 +2,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
-int main()
+void display_usage(int opt)
 {
-    printf("Hello, world!\n");
-    return 0;
+    fprintf(stderr, "Unknown option -%c.\n", optopt);
+    exit(EXIT_FAILURE);
+}
+
+
+int main(int argc, char **argv)
+{
+    int opt = 0;
+    char *time_value = NULL;
+    char *mem_value = NULL;
+    char command[256];
+
+    opterr = 0;
+    while ((opt = getopt(argc, argv, "t:m:")) != -1)
+    {
+        switch (opt)
+        {
+            case 't':
+                time_value = optarg;
+                break;
+            case 'm':
+                mem_value = optarg;
+                break;
+            case '?':
+                display_usage(opt);
+                break;
+            default:
+                break;
+        }
+    }
+
+    int i = 0;
+    for (i=optind; i<argc; i++)
+    {
+        strcat(command, " ");
+        strcat(command, argv[i]);
+    }
+
+    printf("Time option: %s, Memory option: %s, Command: %s\n",
+        time_value, mem_value, command);
+
+    return EXIT_SUCCESS;
 }
